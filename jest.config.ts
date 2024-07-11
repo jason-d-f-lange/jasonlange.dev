ce490fa948ea258dc40115d1e0b9b92d04a5d7c9
jdlange <62232081+jason-d-f-lange@users.noreply.github.com>
2024-07-11 17:24:08 +1000

    Setup jest the Next.js way

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,14 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const config: Config = {
-  clearMocks: true,
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
-export default config;
+export default createJestConfig(config);
