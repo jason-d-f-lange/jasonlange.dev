582c2ff1d47ebd02730b9969574e7c2af8a40490
jdlange <62232081+jason-d-f-lange@users.noreply.github.com>
2024-07-11 20:10:06 +1000

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -21,7 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <Background />
         {children}
+
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
