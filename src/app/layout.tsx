0723825500fe747599bf7aae79ba1206e18220b0
jdlange <62232081+jason-d-f-lange@users.noreply.github.com>
2024-07-11 20:03:58 +1000

    Fix background not showing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
         <Background />
+        {children}
         <SpeedInsights />
       </body>
     </html>
